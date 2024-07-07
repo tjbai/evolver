fix token calculation and turn all_tokens into a feature flag

data.py
@@ -60,19 +60,23 @@ def get_input_ids(trajectory, max_len, tokenizer):
         return_tensors='pt'
     )['input_ids']
     
+def sum_tokens(traj_input_ids):
+    return sum(torch.sum(traj[-1] != PAD_TOKEN_ID) for traj in traj_input_ids)
+    
 def collate_unsupervised(x):
     traj_input_ids, log_probs = zip(*x)
+    toks = sum_tokens(traj_input_ids)
     T = max(traj.shape[0] for traj in traj_input_ids)
     traj_input_ids = torch.stack([pad_traj_input_ids(traj, T) for traj in traj_input_ids])
     log_probs = torch.tensor(log_probs)
-    return traj_input_ids, log_probs, None
+    return traj_input_ids, log_probs, None, toks
 
 def collate_supervised(x):
-    traj_input_ids, traj_edit_tgts = zip(*x)
     T = max(traj.shape[0] for traj in traj_input_ids)
+    toks = sum_tokens(traj_input_ids)
     traj_input_ids = torch.stack([pad_traj_input_ids(traj, T) for traj in traj_input_ids])
     traj_edit_tgts = [pad_traj_edit_tgts(tgts, T) for tgts in traj_edit_tgts]
-    return traj_input_ids, None, tuple(map(lambda x: torch.stack(x), zip(*traj_edit_tgts)))
+    return traj_input_ids, None, tuple(map(lambda x: torch.stack(x), zip(*traj_edit_tgts))), toks
     
 class TrajectoryDataset(Dataset):
     
@@ -104,8 +108,12 @@ class TrajectoryDataset(Dataset):
     
 class SupervisedTrajectoryDataset(TrajectoryDataset):
 
-    def __init__(self, traj_list, log_probs, max_len, tokenizer, limit=None, cache_prefix=None):
+    def __init__(
+        self, traj_list, log_probs, max_len, tokenizer,
+        limit=None, cache_prefix=None, all_tokens=False
+    ):
         super().__init__(traj_list, log_probs, max_len, tokenizer, limit)
+        self.all_tokens = all_tokens
         
         aligner = SentenceAligner(
             model='bert-base-uncased',
@@ -132,10 +140,12 @@ class SupervisedTrajectoryDataset(TrajectoryDataset):
         with open(f'{self.cache_path}/{self.cache_prefix}_{idx}.zst', 'rb') as f:
             op_tgts, tok_tgts, idx_tgts = pickle.load(f)
             
+            tok_tgts = F.one_hot(tok_tgts, VOCAB_SIZE) if self.all_tokens else \
+                       F.one_hot(self.traj_input_ids[idx], VOCAB_SIZE)[1:]
+            
             return self.traj_input_ids[idx], (
                 F.one_hot(op_tgts, 5),
-                # F.one_hot(tok_tgts, VOCAB_SIZE),
-                F.one_hot(self.traj_input_ids[idx], VOCAB_SIZE)[1:],
+                tok_tgts,
                 F.one_hot(idx_tgts, self.max_len)
             )
     

train.py
@@ -70,7 +70,7 @@ def train_evolver(
     num_particles=None, threshold=None, temperature=1.0, resample_at=1,
     device='cuda', name='test', start_step=0
 ):
-    for step, (traj_input_ids, _, traj_edit_tgts) in tqdm(
+    for step, (traj_input_ids, _, traj_edit_tgts, _) in tqdm(
         enumerate(train_loader, start=start_step),
         total=train_steps
     ):
@@ -140,24 +140,21 @@ def evolver_elbo(evolver, eval_loader, eval_steps, device):
     tot_loss = 0
     tot_n = 0
     
-    for step, (traj_input_ids, log_posterior, _) in enumerate(eval_loader):
+    for step, (traj_input_ids, log_posterior, _, n) in enumerate(eval_loader):
         if step >= eval_steps: break
         
         traj_input_ids = traj_input_ids.to(device)
         log_posterior = log_posterior.to(device)
         
-        _, log_likelihood = sample_trajectory(
-            evolver, traj_input_ids,
-            num_particles=1, threshold=0, temperature=0.5, resample_at=1e9
-        )
-        
+        _, log_likelihood = sample_trajectory(evolver, traj_input_ids, num_particles=1, temperature=0.5)
         tot_loss += torch.sum(log_likelihood - log_posterior)
-        tot_n += torch.sum(traj_input_ids[:, -1] != PAD_TOKEN_ID)
+        tot_n += n
        
     return tot_loss / tot_n
 
 def train_ar(
-    model, optim, lr_scheduler, train_loader, eval_loader,
+    model, optim, lr_scheduler,
+    train_loader, eval_loader,
     train_steps, eval_steps, grad_accum_steps,
     checkpoint_at, eval_at,
     device, name,
@@ -223,16 +220,15 @@ def ar_elbo(model, eval_loader: TrajectoryDataset, eval_steps, device):
     tot_loss = 0
     tot_n = 0
     
-    for step, (traj_input_ids, log_posterior, _) in enumerate(eval_loader):
+    for step, (traj_input_ids, log_posterior, _, n) in enumerate(eval_loader):
         if step >= eval_steps: break
         
         traj_input_ids = traj_input_ids.to(device)
         log_posterior = log_posterior.to(device)
         
         log_likelihood = traj_likelihood(model, traj_input_ids)
-        
-        tot_loss += torch.sum(log_likelihood - log_posterior)
-        tot_n += torch.sum(traj_input_ids[:, -1] != PAD_TOKEN_ID)
+        tot_loss += log_likelihood - torch.sum(log_posterior)
+        tot_n += n
        
     return tot_loss / tot_n
 
@@ -430,7 +426,8 @@ def main():
             path=config['train'],
             max_len=config['max_len'],
             tokenizer=tokenizer,
-            cache_prefix=prefix
+            cache_prefix=prefix,
+            all_tokens=config['all_tokens']
         )
         
         train_loader = DataLoader(
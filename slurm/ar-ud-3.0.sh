94b3d85a93284355db48d34ff967ab84a0b795d6
TJ Bai <tjianbai@gmail.com>
2024-07-06 21:56:02 -0400

    add slurm scripts

---
+++ slurm/ar-ud-3.0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=a100
+#SBATCH -A jeisner1_gpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=8:00:0
+#SBATCH --job-name=ar-ud-3.0
+#SBATCH --output=ar-ud-3.0.out
+#SBATCH --mem=80G
+
+ml anaconda
+conda activate evo
+python3 train.py --config configs/ud-3/ar-ud-3.0.json --device cuda
